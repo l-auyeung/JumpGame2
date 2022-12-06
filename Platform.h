c726d6d1b46cedccce40edf7b7003899f4768153
raniyahassan <112319297+raniyahassan@users.noreply.github.com>
2022-12-06 07:51:03 -0800

    Create Platform.h

---
+++ Platform.h
@@ -0,0 +1,23 @@
+#ifndef PLATFORM
+#define PLATFORM
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+using namespace sf;
+
+class Platform
+{
+    private:
+        Vector2f m_position;
+        Sprite platform;
+    public:
+        Platform();
+        Sprite returnPlat();
+        FloatRect getBounds(); 
+        Vector2f getPosition();
+        vector<Sprite> generator(); 
+};
+
+#endif
